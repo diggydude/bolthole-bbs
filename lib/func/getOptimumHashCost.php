581222b969e805841cc53d4ad47c6be4623dc842
Diggy Dude <33551356+diggydude@users.noreply.github.com>
2019-04-19 16:23:07 -0500

    Add files via upload

---
+++ lib/func/getOptimumHashCost.php
@@ -0,0 +1,18 @@
+<?php
+
+  getOptimumHashCost($target = null, $algorithm = PASSWORD_BCRYPT)
+  {
+    if ($target == null) {
+      $target = 0.05;
+    }
+    $cost = 8;
+    do {
+      $cost++;
+      $start = microtime(true);
+      password_hash("test", $algorithm, ['cost' => $cost]);
+      $end = microtime(true);
+    } while (($end - $start) < $target);
+    return $cost;
+  } // getOptimumHashCost
+
+?>
